59abbee49d3a0aa02a232c781c75e9ee5eefeb9f
Simon Moore <simonmoore23@gmail.com>
2023-12-14 09:59:01 +0000

    htmlRoutes and server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Express middleware will always run the operation in the order from top to bottom "order matters"
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use('/', htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
